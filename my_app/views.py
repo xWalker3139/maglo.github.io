bcf4f9768e7cff69dbadccdde14fad2a268a792d
xWalker3139 <razvan.zoriba@yahoo.com>
2021-12-18 13:45:13 +0200

    maglo

--- my_app/views.py
+++ my_app/views.py
@@ -232,9 +232,10 @@ def autentificate_copil(request):
         username = request.POST.get('username')
         password = request.POST.get('password1')
         user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('acasa_copil')
+        if user:
+            if user.is_active():
+                login(request, user)
+                return redirect('acasa_copil')
         else:
             messages.warning(request, "Numele sau parola este incorecta!")
     return render(request, "my_app/autentificate_copil.html", {'date_posted':date_posted})
